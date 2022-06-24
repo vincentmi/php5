32fcb88ab08245031c64325a3f6456929d469a2f
Vincent Mi <miwenshu@gmail.com>
2022-06-24 22:50:47 +0800

    fix

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker build -t php5tcc .
+docker build -t vincentmi/php5 .
--- run.sh
+++ run.sh
@@ -2,3 +2,7 @@
 
 docker run --rm -i -t --name tcc -v `pwd`/tcc:/var/www/public \
 -v `pwd`/log:/var/log/nginx -p 30080:80 php5tcc
+
+
+
+docker run --rm -i -t --name tcc -v `pwd`/log:/var/log/nginx -p 30080:80 tcc2
